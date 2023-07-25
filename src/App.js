6cbec7cc30652b57a822e3325c81188384fbbd11
gabrielagi <gabrielagi503@gmail.com>
2023-07-25 15:55:11 -0300

    Search a specific character by their id in the API

--- src/App.js
+++ src/App.js
@@ -2,25 +2,42 @@ import "./App.css";
 import Cards from "./components/Cards.jsx";
 import Nav from "./components/Nav";
 import { useState } from "react";
+import axios from "axios";
 
 function App() {
   const [characters, setCharacter] = useState([]);
 
-  const example = {
-    id: 1,
-    name: "Rick Sanchez",
-    status: "Alive",
-    species: "Human",
-    gender: "Male",
-    origin: {
-      name: "Earth (C-137)",
-      url: "https://rickandmortyapi.com/api/location/1",
-    },
-    image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
-  };
+  //------Ejercicio 1 a 6------
+  //   const example = {
+  //     id: 1,
+  //     name: "Rick Sanchez",
+  //     status: "Alive",
+  //     species: "Human",
+  //     gender: "Male",
+  //     origin: {
+  //       name: "Earth (C-137)",
+  //       url: "https://rickandmortyapi.com/api/location/1",
+  //     },
+  //     image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+  //   };
 
-  const onSearch = () => {
-    setCharacter([...characters, example]);
+  //   const onSearch = () => {
+  //     setCharacter([...characters, example]);
+  //   };
+  //------Ejercicio 1 a 5------
+
+  const onSearch = (id) => {
+    //function onSearch(id) {
+    axios(`https://rickandmortyapi.com/api/character/${id}`).then(
+      //id es mi estado local que el usuario escribe en el input
+      ({ data }) => {
+        if (data.name) {
+          setCharacter((oldChars) => [...oldChars, data]);
+        } else {
+          alert("¡No hay personajes con este ID!");
+        }
+      }
+    );
   };
 
   return (
@@ -30,5 +47,4 @@ function App() {
     </div>
   );
 }
-
 export default App;

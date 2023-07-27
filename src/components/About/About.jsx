25782dbfb17de87139445b2a62e6ad20b3a5d7cd
gabrielagi <gabrielagi503@gmail.com>
2023-07-27 18:31:54 -0300

    Styles About Profile

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -18,42 +18,46 @@ const About = () => {
           src="https://avatars.githubusercontent.com/u/48634426?s=400&u=a8e43bd90c72385e8e7cc5b1e3f3cf9b2ba34f2f&v=4"
           alt="Gif 1"
         />
-        <h3>Nombre del Desarrollador(a)</h3>
+        <h3>Gabriela Iriart</h3>
         <p>
-          Aquí puedes escribir una descripción sobre la desarrolladora web full
-          stack. Puedes mencionar sus habilidades, experiencia y cualquier otra
-          información relevante.
+          ¡Hola! Soy una desarrolladora de software Full Stack apasionada por
+          crear soluciones tecnológicas innovadoras que mejoren la experiencia
+          del usuario, enfocada en el diseño centrado en el usuario y la
+          usabilidad, buscando el equilibrio perfecto entre funcionalidad y
+          estética en cada proyecto.
         </p>
         <GifWrapper>
           {/* <Gif
             src="https://media.giphy.com/media/du3J3cXyzhj75IOgvA/giphy.gif"
             alt="Gif 1"
           /> */}
-          <Gif
-            src="https://www.svgrepo.com/show/512317/github-142.svg"
-            alt="Github"
-          />
-          <Gif
-            src="https://www.svgrepo.com/show/521725/linkedin.svg"
-            alt="LinkedIn"
-          />
-          <Gif
-            src="https://www.svgrepo.com/show/15898/behance.svg"
-            alt="Behance"
-          />
-          <Gif
-            src="https://www.svgrepo.com/show/488179/email.svg"
-            alt="Email"
-          />
-          {/* Agrega más gifs animados aquí si lo deseas */}
+          {/* Agrega más gifs animados aquí */}
         </GifWrapper>
         <LinksList>
-          <NavLink to="/about">
-            <button>About</button>
-          </NavLink>
-          <NavLink to="/home">
-            <button>Home</button>
-          </NavLink>
+          <a href="https://github.com/gabrielagi">
+            <Gif
+              src="https://www.svgrepo.com/show/512317/github-142.svg"
+              alt="Github"
+            />
+          </a>
+          <a href="https://www.linkedin.com/in/gabriela-iriart/">
+            <Gif
+              src="https://www.svgrepo.com/show/521725/linkedin.svg"
+              alt="LinkedIn"
+            />
+          </a>
+          <a href="https://www.behance.net/gabrielairiart">
+            <Gif
+              src="https://www.svgrepo.com/show/15898/behance.svg"
+              alt="Behance"
+            />
+          </a>
+          <a href="https://www.linkedin.com/in/gabriela-iriart/">
+            <Gif
+              src="https://www.svgrepo.com/show/488179/email.svg"
+              alt="Email"
+            />
+          </a>
         </LinksList>
       </ProfileInfo>
     </ProfileContainer>

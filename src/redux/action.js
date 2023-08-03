91b2a56025f25851891f2db0f4943e9a57fcf98d
gabrielagi <gabrielagi503@gmail.com>
2023-08-03 14:52:11 -0300

    New Actions FILTER ORDER

--- src/redux/action.js
+++ src/redux/action.js
@@ -1,4 +1,4 @@
-import { ADD_FAV, REMOVE_FAV } from "./action-types";
+import { ADD_FAV, REMOVE_FAV, FILTER, ORDER } from "./action-types";
 
 export const addFav = (character) => {
   return { type: ADD_FAV, payload: character };
@@ -6,4 +6,12 @@ export const addFav = (character) => {
 
 export const removeFav = (id) => {
   return { type: REMOVE_FAV, payload: id };
+};
+
+export const filter = (gender) => {
+  return { type: FILTER, payload: gender };
+};
+
+export const order = (order) => {
+  return { type: ORDER, payload: order };
 };

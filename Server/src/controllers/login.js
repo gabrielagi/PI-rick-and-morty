1722ff983ea97882ac44af493511f1a859fff3ad
gabrielagi <gabrielagi503@gmail.com>
2023-08-15 12:05:14 -0300

    New Login method

---
+++ Server/src/controllers/login.js
@@ -0,0 +1,15 @@
+const user = require("../utils/users");
+
+const login = (req, res) => {
+  const { email, password } = req.query;
+
+  const userFound = user.find(
+    (user) => user.email === email && user.password === password
+  );
+
+  return userFound
+    ? res.status(200).json({ access: true })
+    : res.status(200).json({ access: false });
+};
+
+module.exports = { login };
---
+++ Server/src/utils/users.js
@@ -0,0 +1,1 @@
+module.exports = [{ email: "gabriela@gmail.com", password: "asd123" }];

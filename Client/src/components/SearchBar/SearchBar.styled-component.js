8a51df40d7d40f98f67ecd6db65db258b0a4b533
gabrielagi <gabrielagi503@gmail.com>
2023-08-26 12:49:21 -0300

    SearchBar - Fixed Styles

--- Client/src/components/SearchBar/SearchBar.styled-component.js
+++ Client/src/components/SearchBar/SearchBar.styled-component.js
@@ -1,28 +1,54 @@
 import styled from "styled-components";
 
 export const SearchContainer = styled.div`
-  display: flex; /* Utilizamos flex para organizar los elementos en fila */
-  align-items: center; /* Centramos verticalmente los elementos */
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const SearchButton = styled.button`
-  background-color: #e545ee;
-  color: #000; /* Cambiar el color del texto a negro */
-  font-size: 12px; /* Ajustar el tamaño de la fuente */
-  padding: 5px 10px; /* Ajustar el padding para hacer el botón más pequeño */
+  background-color: transparent;
+  margin: 5px 0px 0px;
+  color: #ff44cc;
+  font-size: 12px;
+  padding: 6px 18px;
   border: none;
-  border-radius: 4px;
+  border-radius: 4px 4px 4px 4px;
   cursor: pointer;
   transition: background-color 0.3s;
 
+  border: 1px solid #ff44cc;
+
   &:hover {
-    background-color: #f461ff;
+    color: #fff;
+    box-shadow: 0 0 10px 2px rgba(255, 105, 180, 0.5);
   }
 `;
 
 export const SearchInput = styled.input`
-  border: 1px solid #ccc;
-  border-radius: 4px;
+  border: 1.5px solid #1d252d;
+  border-radius: 4px 4px 4px 4px;
+  margin: 5px 0px 0px;
+  height: 25px;
   font-size: 16px;
-  color: #000;
+  padding-left: 4px;
+  color: #fff;
+  background-color: rgba(255, 255, 255, 0.1);
+  transition: border-color 0.3s ease-in-out;
+
+  &::placeholder {
+    font-size: 13px; /* Tamaño de las letras del placeholder */
+    padding-left: 4px;
+    color: rgba(
+      255,
+      255,
+      255,
+      0.5
+    ); /* Color semi traslúcido para el placeholder */
+  }
+
+  &:focus {
+    outline: none;
+    border: 1px solid #615f60;
+  }
 `;
